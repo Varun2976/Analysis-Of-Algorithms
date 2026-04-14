a7cb760673ccc8cc778d92a250051a46cc59c236
Varun2976 <vsnair2976@gmail.com>
2026-04-14 12:49:14 +0530

    Job sequencing with deadlines code

--- MOD_3/Job_Sequencing.cpp
+++ MOD_3/Job_Sequencing.cpp
@@ -1,28 +1,65 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-struct job{
-    int id , deadline , profit;
+struct Job {
+    int id, deadline, profit;
 };
 
-void Job_Sequencing(vector<int>&arr , int n){
-    
+bool cmp(Job a, Job b) {
+    return a.profit > b.profit;
 }
-int main(){
 
+// Function
+pair<vector<int>, int> jobSequencing(vector<Job>& jobs) {
+    sort(jobs.begin(), jobs.end(), cmp);
+
+    int maxDeadline = 0;
+    for (auto j : jobs)
+        maxDeadline = max(maxDeadline, j.deadline);
+
+    vector<int> slot(maxDeadline + 1, -1);
+    int totalProfit = 0;
+
+    for (auto j : jobs) {
+        for (int t = j.deadline; t > 0; t--) {
+            if (slot[t] == -1) {
+                slot[t] = j.id;
+                totalProfit += j.profit;
+                break;
+            }
+        }
+    }
+
+    vector<int> result;
+    for (int i = 1; i <= maxDeadline; i++) {
+        if (slot[i] != -1)
+            result.push_back(slot[i]);
+    }
+
+    return {result, totalProfit};
+}
+
+// 🔹 Main Function
+int main() {
     int n;
+    cout << "Enter number of jobs: ";
     cin >> n;
 
-    vector<int>arr(n);
+    vector<Job> jobs(n);
 
-    for(int i = 0 ; i < n ;i++){
-        cin >> arr[i];
+    cout << "Enter id, deadline, profit for each job:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> jobs[i].id >> jobs[i].deadline >> jobs[i].profit;
     }
 
-    Job_Sequencing(arr,n);
+    auto ans = jobSequencing(jobs);
 
-    for(int c : arr){
-        cout << c << " ";
+    cout << "\nSelected Jobs: ";
+    for (int id : ans.first) {
+        cout << id << " ";
     }
-    cout << endl;
+
+    cout << "\nTotal Profit: " << ans.second << endl;
+
     return 0;
 }
