50ab651f47e37b33139b890c6f1166d74d845181
Varun2976 <vsnair2976@gmail.com>
2026-05-08 09:38:12 +0530

    Merge sort complete

--- Practice_Paper/practice.cpp
+++ Practice_Paper/practice.cpp
@@ -5,11 +5,26 @@ void Merge(vector<int> &a , int low , int high){
     int i = low;
     int j = mid +1;
     int k = low;
-
+    vector
     while(i <= mid && j <= high){
-        if(A[i] < A[j]){
-            T[k] = A[i]
+        if(a[i] < a[j]){
+            T[k] = a[i];
+            i= i + 1;
+        }else{
+            T[k] = a[j];
+            j= j+1;
         }
+        k=k+1;
+    }
+    while(i <= mid){
+        T[k] = a[i];
+        i= i+1;
+        k=k+1;
+    }
+    while(j <= high){
+        T[k] = a[j];
+        j = j + 1;
+        k = k + 1;
     }
 }
 void Merge_Sort(vector<int> &arr,int n,int low,int high,int mid){
