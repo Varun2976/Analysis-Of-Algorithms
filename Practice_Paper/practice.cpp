54a996f55590d18605bfc4120167ddd377b79460
Varun2976 <vsnair2976@gmail.com>
2026-05-08 09:33:09 +0530

    Merge initialized

--- Practice_Paper/practice.cpp
+++ Practice_Paper/practice.cpp
@@ -1,26 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void Selection(vector<int> &arr,int n){
-    int maxim = *max_element(arr.begin(),arr.end());
+void Merge(vector<int> &a , int low , int high){
+    int i = low;
+    int j = mid +1;
+    int k = low;
 
-    vector<int>count(maxim+1,0);
-    vector<int>output(n);
-
-    for(int i = 0 ; i < n ; i++){
-        count[arr[i]]++;
+    while(i <= mid && j <= high){
+        if(A[i] < A[j]){
+            T[k] = A[i]
+        }
     }
-    for(int i=1;i < n;i++){
-        count[i] += count[i-1];
-    }
-    for(int i = maxim ; i > 0 ;i--){
-        count[i] = count[i-1];
-    }
-    count[0] = 0;
-
-    for(int i = 0 ; i < n;i++){
-        output[count[arr[i]]] = arr[i];
-        count[arr[i]]++;
+}
+void Merge_Sort(vector<int> &arr,int n,int low,int high,int mid){
+    mid = (low+high)/2;
+    if(low < high){
+        Merge_Sort(a,low,mid);
+        Merge_Sort(a,mid+1,high);
+        Merge();
     }
 }
 int main(){
@@ -33,7 +30,7 @@ int main(){
         cin >> arr[i];
     }
 
-    Insertion_Sort(arr,n);
+    Merge_Sort(arr,n);
 
     for(int c : arr){
         cout << c << " ";
